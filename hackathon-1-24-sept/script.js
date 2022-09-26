93bd414f3c78624694a85354b5b44f457843b035
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-09-26 09:06:16 +0530

    stupid updates

--- hackathon-1-24-sept/script.js
+++ hackathon-1-24-sept/script.js
@@ -1,20 +1,77 @@
-const ip = document.querySelector('#input')
+const ip = document.querySelector('#note-input')
 const submit = document.querySelector('#submit')
 const ipForm = document.querySelector('#input-form')
+const viewDiv= document.querySelector('#view-notes-section')
 
-const displayNotes = document.querySelector('#display-notes')
+const noteFwd = document.querySelector('#toggle-note-fwd')
+const noteBack = document.querySelector('#toggle-note-back')
+
+const displayNotes = document.querySelector('.display-notes')
 
 
+const renderList = (obj)=> {
 
 
-const noteList = [];
+    console.log("obj", obj);
+    let div= document.createElement('div');
+    let para= document.createElement('p');
+    let span1= document.createElement('div');
+    let span2= document.createElement('div');
+    let textInfoDiv= document.createElement('div');
+
+    let clipboard= document.createElement('button');
+    
+    para.innerText = obj.text;
+    para.contentEditable= "true";
+    span1.innerText =  `By ${obj.Author}`  
+    span2.innerText =  obj.date 
+
+    clipboard.innerText="📋";
+    clipboard.addEventListener('click', (e)=> {
+        navigator.clipboard.writeText(para.innerText)
+    })
+
+    div.id= "note-container"
+    textInfoDiv.id="note-info"
+    para.id= "note-text"
+    span1.id= "date-text"
+    span2.id= "author-text"
+    clipboard.id= "cc-btn"
+
+    span2.style.fontWeight= "800"
+    div.appendChild(para);
+    textInfoDiv.appendChild(span1);
+    textInfoDiv.appendChild(span2);
+    div.appendChild(textInfoDiv);
+    div.appendChild(clipboard);
+    displayNotes.appendChild(div);
+
+}
+
+
+let noteList = [];
+
+let savedData = JSON.parse(localStorage.getItem('note-list')) || null
+if(savedData){
+
+    noteList= savedData;
+
+    for(let obj of noteList){
+        renderList(obj);
+    }
+    
+    localStorage.removeItem("note-list")
+
+}
 
 ipForm.addEventListener('submit', (e)=> {
     e.preventDefault()
     let words = ip.value;
+    let todayDate= new Date();
+    let formattedDate = todayDate.getHours() + ":" +todayDate.getMinutes() + " "+ todayDate.toDateString(); 
     const noteObject = {
         text: "", 
-        date: new Date(),
+        date: formattedDate,
         Author: "Antariksh"
     }
 
@@ -22,6 +79,7 @@ ipForm.addEventListener('submit', (e)=> {
     noteList.push(noteObject)
     ip.value=""
 
+    //localStorage.removeItem("note-list")
     localStorage.setItem("note-list", JSON.stringify(noteList))
 
     
@@ -39,73 +97,19 @@ ipForm.addEventListener('submit', (e)=> {
 
 
 
-//---------------------LIST-RENDER---------------------
+//---------------------view notes---------------------
 
+let iter=0;
+console.log("YESSS",typeof displayNotes[iter])
+viewDiv.appendChild(displayNotes[iter])
 
-
-const renderList = (obj)=> {
-
-    let savedData = JSON.parse(localStorage.getItem('note-list')) || null
-    if(savedData){
-        let noteList = savedData;
+noteBack.addEventListener('click', (e)=> {
+    if(iter!=0){
+        iter--;
     }
-
-
-
-  //  for(let obj of noteList){
-        console.log("obj", obj);
-        let div= document.createElement('div');
-        let para= document.createElement('p');
-        let span1= document.createElement('span');
-        let span2= document.createElement('span');
-        let textInfoDiv= document.createElement('div');
-
-        let clipboard= document.createElement('button');
-        
-        
-        para.innerText = obj.text;
-
-
-        let initialtext= obj.text;
-
-        
-
-
-
-
-
-
-
-
-
-        span1.innerText = obj.date.getHours() + ":" +obj.date.getMinutes() + " "+ obj.date.toDateString(); 
-
-        span2.innerText = obj.Author
-
-        clipboard.innerText="📋"
-        clipboard.addEventListener('click', (e)=> {
-            navigator.clipboard.writeText(para.innerText)
-        })
-
-
-        div.id= "note-container"
-        textInfoDiv.id="note-info"
-        para.id= "note-text"
-        span1.id= "date-text"
-        span2.id= "author-text"
-        clipboard.id= "cc-btn"
-
-
-        span2.style.fontWeight= "800"
-        div.appendChild(para);
-        textInfoDiv.appendChild(span1);
-        textInfoDiv.appendChild(span2);
-        div.appendChild(textInfoDiv);
-        div.appendChild(clipboard);
-
-
-
-        displayNotes.appendChild(div);
-    
-  //  }
-}
+})
+noteBack.addEventListener('click', (e)=> {
+    if(iter!=displayNotes.length-1){
+        iter++;
+    }
+})
